d516490b3615ce12d82943ee2a1d650b13a784ca
Deepak Guptha <vzan2012@gmail.com>
2019-05-01 13:56:01 +0530

    feat(user): Updated the scripts and html file to perform calculations

---
+++ app.js
@@ -0,0 +1,65 @@
+// Custom Scripts - Start
+
+document
+  .querySelector("#loan-form")
+  .addEventListener("submit", calculateResults);
+
+function calculateResults(e) {
+  console.log("Calculating...");
+
+  // Define the UI vars
+  const amount = document.querySelector("#amount");
+  const interest = document.querySelector("#interest");
+  const years = document.querySelector("#years");
+
+  const monthlyPayment = document.querySelector("#monthly-payment");
+  const totalPayment = document.querySelector("#total-payment");
+  const totalInterest = document.querySelector("#total-interest");
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  // Compute monthly payment
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    // Custom Error Function
+    showError("Please check your numbers");
+  }
+
+  e.preventDefault();
+}
+
+// showError function
+function showError(error) {
+  // Create element
+  const errorDiv = document.createElement("div");
+
+  // Add the alert classes (bootstrap)
+  errorDiv.className = "alert alert-danger";
+
+  // Create a text node and append to the div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Get the elements
+  const card = document.querySelector(".card");
+  const heading = document.querySelector(".heading");
+
+  // Inserting the error above the heading
+  card.insertBefore(errorDiv, heading);
+  
+  // Clear the error after 3 seconds
+  setTimeout(clearError, 3000);
+
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
+// Custom Scripts - End
